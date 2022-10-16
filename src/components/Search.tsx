d70e62c019db54d9e20fd5d72e623eec585d745b
Ladislav Burgr <burgr.ladislav@gmail.com>
2022-10-16 21:07:23 +0200

    ui draft

---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, TextField, Button} from '@mui/material/';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+export default function BasicTable() {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'center' }}>
+            <GitHubIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField id="input-with-sx" label="Username" variant="standard" />
+            <Button variant="contained" sx={{ ml: 2}}>Search</Button>
+        </Box>
+)};
---
+++ src/components/UserHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+
+export default function BasicTable() {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', my: "1rem" }}>
+            <Avatar alt="User User" src="/static/images/avatar/1.jpg" />
+            <span style={{ marginLeft: ".5rem" }}>User User</span>
+        </Box>
+
+)};
